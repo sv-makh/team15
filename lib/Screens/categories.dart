67f4919948affcbeb5284b0664e26265548fea31
sv-makh <mithriam@mail.ru>
2022-05-07 14:23:11 +0300

    add bottom navigation bar and widget files

---
+++ lib/Screens/categories.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Categories {
+
+  Widget categories() {
+    return Text(
+      'Index 0: Categories',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    );
+  }
+
+}
---
+++ lib/Screens/profile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Profile {
+
+  Widget profile() {
+    return Text(
+      'Index 2: Profile',
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    );
+  }
+
+}
