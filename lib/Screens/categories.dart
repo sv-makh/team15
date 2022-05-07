e3bd1487be7b785264109e6b282a33c4daf0ecc0
Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>
2022-05-07 17:56:46 +0300

    fixed profile page
    Signed-off-by: Ekaterina Medvedeva <e.v.medvedeva.1996@yandex.ru>

--- lib/Screens/categories.dart
+++ lib/Screens/categories.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 
 class Categories {
   Widget categories() {
+    return CategoriesList();
+  }
+}
+
+class CategoriesList extends StatefulWidget {
+  const CategoriesList({Key? key}) : super(key: key);
+
+  @override
+  State<CategoriesList> createState() => _CategoriesListState();
+}
+
+class _CategoriesListState extends State<CategoriesList> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
             child: Center(
@@ -116,7 +130,10 @@ class Categories {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(38.0),
               ),
-              onPressed: () {},
+              onPressed: () {
+                Route route = MaterialPageRoute(builder: ((context) => Prog()));
+                Navigator.push(context, route);
+              },
               padding: EdgeInsets.all(10.0),
               color: Color.fromRGBO(126, 239, 220, 1),
               textColor: Colors.white,
@@ -124,6 +141,71 @@ class Categories {
                   style: TextStyle(fontSize: 28, color: Colors.black)),
             ),
           ),
+        ]))));
+  }
+}
+
+class Prog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            width: 356,
+            height: 80,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(38.0),
+              ),
+              onPressed: () {},
+              padding: EdgeInsets.all(10.0),
+              color: Color.fromRGBO(126, 239, 220, 1),
+              textColor: Colors.white,
+              child: Text("Веб-разработка",
+                  style: TextStyle(fontSize: 28, color: Colors.black)),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            width: 356,
+            height: 80,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(38.0),
+              ),
+              onPressed: () {},
+              padding: EdgeInsets.all(10.0),
+              color: Color.fromRGBO(126, 239, 220, 1),
+              textColor: Colors.white,
+              child: Text("Мобильная разработка",
+                  style: TextStyle(fontSize: 28, color: Colors.black)),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            width: 356,
+            height: 80,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(38.0),
+              ),
+              onPressed: () {},
+              padding: EdgeInsets.all(10.0),
+              color: Color.fromRGBO(126, 239, 220, 1),
+              textColor: Colors.white,
+              child: Text("Аналитика",
+                  style: TextStyle(fontSize: 28, color: Colors.black)),
+            ),
+          )
         ]))));
   }
 }
--- lib/Screens/profile.dart
+++ lib/Screens/profile.dart
@@ -12,7 +12,8 @@ class User extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+        body: SingleChildScrollView(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
@@ -38,7 +39,7 @@ class User extends StatelessWidget {
             padding: EdgeInsets.all(10.0),
             margin: EdgeInsets.all(10.0),
             height: 100,
-            width: 350,
+            width: 400,
             child: Text(
               'Путешествия, спорт, здоровый образ жизни. Изучаю разработку, планирую создать свою мобильную игру.',
               style: TextStyle(fontSize: 18),
@@ -52,11 +53,92 @@ class User extends StatelessWidget {
           SizedBox(
             height: 10,
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 50,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(38.0),
+                  ),
+                  onPressed: () {},
+                  padding: EdgeInsets.all(10.0),
+                  color: Color.fromRGBO(119, 235, 151, 1),
+                  textColor: Colors.white,
+                  child: Text("Дизайн",
+                      style: TextStyle(fontSize: 18, color: Colors.black)),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 50,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(38.0),
+                  ),
+                  onPressed: () {},
+                  padding: EdgeInsets.all(10.0),
+                  color: Color.fromRGBO(126, 239, 220, 1),
+                  textColor: Colors.white,
+                  child: Text("Программирование",
+                      style: TextStyle(fontSize: 18, color: Colors.black)),
+                ),
+              ),
+            ],
+          ),
           Text('Мои лекции',
               style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-          TextButton(onPressed: () {}, child: Text('Настройки'))
+          SizedBox(
+            height: 10,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 50,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(38.0),
+                  ),
+                  onPressed: () {},
+                  padding: EdgeInsets.all(10.0),
+                  color: Color.fromRGBO(126, 239, 220, 1),
+                  textColor: Colors.white,
+                  child: Text("Создание макетов в Figma",
+                      style: TextStyle(fontSize: 18, color: Colors.black)),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 50,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(38.0),
+                  ),
+                  onPressed: () {},
+                  padding: EdgeInsets.all(10.0),
+                  color: Color.fromRGBO(126, 239, 220, 1),
+                  textColor: Colors.white,
+                  child: Text("Flutter для начинающих",
+                      style: TextStyle(fontSize: 18, color: Colors.black)),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              style: ButtonStyle(),
+              onPressed: () {},
+              child: Text(
+                'Настройки',
+              ))
         ],
       ),
-    );
+    ));
   }
 }
